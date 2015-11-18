1259fe89561c4f873460dff8736117ea70a2a61c
PFENIGA1 <PFENIGA1@PHCZPR-L50036.EU.novartis.net>
2015-11-18 16:27:37 +0100

    thing are changed

--- src/generatestuffs/ReadFile3.java
+++ src/generatestuffs/ReadFile3.java
@@ -8,15 +8,42 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
 
-public class ReadFile2 {
+public class ReadFile3 {
 
     public static void main(String[] args) {
 
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\NetBeansProjects\\GenerateStuffs\\src\\generatestuffs\\testfile.txt"))){
 
             String row;
-
             StringWriter sql = new StringWriter();
+            
+                public static String fs=System.getProperty("file.separator");
+    public static String hd=System.getProperty("user.home");
+
+    public static String dbfolder="dbtest";
+
+    public static String dbname="test.db";
+
+    public static String dbfullpath=hd + fs + dbfolder + fs + dbname;
+
+	public static void main(String[] args) {
+
+		File path = new File(hd + fs + dbfolder);
+
+		if (!path.exists()) {
+                    path.mkdir();
+                    }
+        try{
+		File dbfile = new File(dbfullpath);
+ 		if (dbfile.createNewFile()){
+                    //String sql = null;
+                    //sql = "CREATE TABLE IF NOT EXISTS list (hostname STRING PRIMARY KEY, ip STRING, uid STRING, pass STRING, description STRING, app STRING, tsocks STRING)";
+                    //selectdb.write(sql);                                
+                    }
+	}catch (IOException e) {
+	}
+        }   
+            
                 
             while ((row = br.readLine()) != null) {
 
